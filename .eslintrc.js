a27bebfb77a8d409bc4fe926d85b796d95907a6a
Tommi Lepola <lepola.tommi@gmail.com>
2020-11-06 20:41:10 +0200

    Finish configuration

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended'
+    ],
+    settings: {
+        react: {
+            version: 'detect'
+        }
+    },
+    env: {
+        browser: true,
+        node: true,
+        es6: true
+    },
+    plugins: ['@typescript-eslint', 'react', 'prettier'],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 2018,
+        sourceType: 'module'
+    },
+    rules: {
+        "react/prop-types": "off",
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "prettier/prettier": "error",
+        "@typescript-eslint/interface-name-prefix": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+        "prettier/prettier": ["error", {
+           "endOfLine":"auto"
+        }],
+    },
+    overrides: [
+        // Override some TypeScript rules just for .js files
+        {
+            files: ['*.js'],
+            rules: {
+                '@typescript-eslint/no-var-requires': 'off'
+            }
+        }
+    ]
+};
+  
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,17 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
-}
+    plugins: [
+        'gatsby-plugin-typescript',
+        {
+            resolve: 'gatsby-plugin-eslint',
+            options: {
+                test: /\.js$|\.jsx$|\.ts$|\.tsx$/,
+                exclude: /(node_modules|.cache|public)/,
+                stages: ['develop'],
+                options: {
+                    emitWarning: true,
+                    failOnError: false,
+                },
+            },
+        },
+    ],
+};
