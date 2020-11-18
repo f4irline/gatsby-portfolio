31a98bea930fa3cdb44d515927d9dc5fb91d12a6
Tommi Lepola <lepola.tommi@gmail.com>
2020-11-18 20:40:55 +0200

    Remove Fade from Hero component

--- src/pages/hero/hero.tsx
+++ src/pages/hero/hero.tsx
@@ -7,29 +7,27 @@ import PortfolioContext from '../../context/context';
 export default function Hero(): JSX.Element | null {
     const { hero, socialLinks } = useContext(PortfolioContext);
 
-    return !hero ? null : (
-        <Fade>
-            <div className={styles.heroContainer}>
-                <div className={styles.heroHeader}>
-                    <h1 className="title-huge uppercase">
-                        <Fade left cascade>
-                            Hello
-                        </Fade>
-                    </h1>
-                    <span className={styles.intro}>
-                        My name is {hero?.name}. I&apos;m a&nbsp;
-                        <span className={styles.role}>{hero?.roles[0]}.</span>
-                    </span>
-                </div>
-                <hr />
-                <div className={styles.social}>
-                    {socialLinks.map(link => (
-                        <a href={link.url} key={link.type}>
-                            <img src={link.imgPath} alt={link.alt}></img>
-                        </a>
-                    ))}
-                </div>
+    return (
+        <div className={styles.heroContainer}>
+            <div className={styles.heroHeader}>
+                <h1 className="title-huge uppercase">
+                    <Fade left cascade>
+                        Hello
+                    </Fade>
+                </h1>
+                <span className={styles.intro}>
+                    My name is {hero?.name}. I&apos;m a&nbsp;
+                    <span className={styles.role}>{hero?.roles[0]}.</span>
+                </span>
             </div>
-        </Fade>
+            <hr />
+            <div className={styles.social}>
+                {socialLinks.map(link => (
+                    <a href={link.url} key={link.type}>
+                        <img src={link.imgPath} alt={link.alt}></img>
+                    </a>
+                ))}
+            </div>
+        </div>
     );
 }
