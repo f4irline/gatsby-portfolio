ae91e09ef8378b8512891dfe585f5ffa4ecf74d2
Tommi Lepola <lepola.tommi@gmail.com>
2020-11-18 20:57:32 +0200

    Add another fade

--- src/pages/hero/hero.tsx
+++ src/pages/hero/hero.tsx
@@ -4,30 +4,32 @@ import Fade from 'react-reveal/Fade';
 
 import PortfolioContext from '../../context/context';
 
-export default function Hero(): JSX.Element {
+export default function Hero(): JSX.Element | null {
     const { hero, socialLinks } = useContext(PortfolioContext);
 
-    return (
-        <div className={styles.heroContainer}>
-            <div className={styles.heroHeader}>
-                <h1 className="title-huge uppercase">
-                    <Fade left cascade>
-                        Hello
-                    </Fade>
-                </h1>
-                <span className={styles.intro}>
-                    My name is {hero?.name}. I&apos;m a&nbsp;
-                    <span className={styles.role}>{hero?.roles[0]}.</span>
-                </span>
+    return !hero ? null : (
+        <Fade ssrFade duration={2000}>
+            <div className={styles.heroContainer}>
+                <div className={styles.heroHeader}>
+                    <h1 className="title-huge uppercase">
+                        <Fade left cascade>
+                            Hello
+                        </Fade>
+                    </h1>
+                    <span className={styles.intro}>
+                        My name is {hero?.name}. I&apos;m a&nbsp;
+                        <span className={styles.role}>{hero?.roles[0]}.</span>
+                    </span>
+                </div>
+                <hr />
+                <div className={styles.social}>
+                    {socialLinks.map(link => (
+                        <a href={link.url} key={link.type}>
+                            <img src={link.imgPath} alt={link.alt}></img>
+                        </a>
+                    ))}
+                </div>
             </div>
-            <hr />
-            <div className={styles.social}>
-                {socialLinks.map(link => (
-                    <a href={link.url} key={link.type}>
-                        <img src={link.imgPath} alt={link.alt}></img>
-                    </a>
-                ))}
-            </div>
-        </div>
+        </Fade>
     );
 }
