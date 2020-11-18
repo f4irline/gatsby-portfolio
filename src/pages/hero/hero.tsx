ed897ecb3a93b942a1761928affba5777214cae1
Tommi Lepola <lepola.tommi@gmail.com>
2020-11-18 17:55:56 +0200

    Add fade to hero component. Wait for hero data

--- src/pages/hero/hero.tsx
+++ src/pages/hero/hero.tsx
@@ -8,28 +8,28 @@ export default function Hero(): JSX.Element {
     const { hero, socialLinks } = useContext(PortfolioContext);
 
     return (
-        <div className={styles.heroContainer}>
-            <div className={styles.heroHeader}>
-                <h1 className="title-huge uppercase">
-                    <Fade left cascade>
-                        Hello
-                    </Fade>
-                </h1>
-                <Fade>
+        <Fade>
+            <div className={styles.heroContainer}>
+                <div className={styles.heroHeader}>
+                    <h1 className="title-huge uppercase">
+                        <Fade left cascade>
+                            Hello
+                        </Fade>
+                    </h1>
                     <span className={styles.intro}>
                         My name is {hero?.name}. I&apos;m a&nbsp;
                         <span className={styles.role}>{hero?.roles[0]}.</span>
                     </span>
-                </Fade>
+                </div>
+                <hr />
+                <div className={styles.social}>
+                    {socialLinks.map(link => (
+                        <a href={link.url} key={link.type}>
+                            <img src={link.imgPath} alt={link.alt}></img>
+                        </a>
+                    ))}
+                </div>
             </div>
-            <hr />
-            <div className={styles.social}>
-                {socialLinks.map(link => (
-                    <a href={link.url} key={link.type}>
-                        <img src={link.imgPath} alt={link.alt}></img>
-                    </a>
-                ))}
-            </div>
-        </div>
+        </Fade>
     );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home(): JSX.Element {
 
     return (
         <PortfolioProvider value={{ hero, socialLinks }}>
-            <Hero></Hero>
+            {hero && <Hero></Hero>}
         </PortfolioProvider>
     );
 }
