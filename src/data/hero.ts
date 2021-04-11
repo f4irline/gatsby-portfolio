2dca09ab0dc5da8e624ee2198604ac98857d2211
Tommi Lepola <lepola.tommi@gmail.com>
2021-04-11 11:00:54 +0300

    Remove tryhackme link for inactivity

--- src/data/hero.ts
+++ src/data/hero.ts
@@ -2,7 +2,6 @@ import { HeroData, SocialLink } from '../models';
 import githubIcon from '../../static/img/icons/github.svg';
 import emailIcon from '../../static/img/icons/email.svg';
 import linkedInIcon from '../../static/img/icons/linkedin.svg';
-import tryHackMeIcon from '../../static/img/icons/tryhackme.svg';
 
 const heroData: HeroData = {
     name: 'Tommi',
@@ -27,12 +26,6 @@ const socialLinks: SocialLink[] = [
         imgPath: emailIcon,
         url: 'mailto:lepola.tommi@gmail.com',
         alt: 'Email',
-    },
-    {
-        type: 'tryhackme',
-        imgPath: tryHackMeIcon,
-        url: 'https://tryhackme.com/p/F4irline',
-        alt: 'TryHackMe',
     },
 ];
 
